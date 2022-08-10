0bd6f8964f10717262a7abc0d61bb2d4626ce4e8
prcwzm <prcwzm@gmail.com>
2022-08-10 23:31:14 +0800

    zenowu 20220810 zenowu 入门learning 0.0.12 integer commit

---
+++ go/basicdata.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	unicode()
+}
+
+func unicode() {
+	ascii := 'a'
+	unicode := '国'
+	newlines := '\n'
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)
+	fmt.Printf("%d %[1]c %[1]q\n", unicode)
+	fmt.Printf("%d %[1]c %[1]q\n", newlines)
+}
+
+func basicHex() {
+	o := 0666
+	fmt.Printf("%d %[1]o %#[1]o\n", o)
+	x := int64(0xdeadbeef)
+	fmt.Printf("%d %[1]x %#[1]x %#[1]X\n", x)
+}
+
+func basicInteger() {
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+	fmt.Printf("%08b\n", x)
+	fmt.Printf("%08b\n", y)
+	fmt.Printf("%08b\n", x&y)
+	fmt.Printf("%08b\n", x|y)
+	fmt.Printf("%08b\n", x^y)
+	fmt.Printf("%08b\n", x&^y)
+
+	for i := uint(0); i < 8; i++ {
+		if x&(1<<i) != 0 {
+			fmt.Println(i)
+		}
+	}
+	fmt.Printf("%08b\n", x<<1)
+	fmt.Printf("%08b\n", x>>1)
+}
